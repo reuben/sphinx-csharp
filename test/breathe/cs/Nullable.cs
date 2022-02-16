81a5cc95a1eb6f4fcb326600706fca6a87a182c6
Roger Barton <rbarton@student.ethz.ch>
2022-02-16 17:18:08 +0100

    More tests on nullables

--- test/breathe/cs/Nullable.cs
+++ test/breathe/cs/Nullable.cs
@@ -23,10 +23,45 @@ namespace OtherNamespace
         /// A nullable function.
         /// </summary>
         /// <param name="bValue">A nullable MyClass.</param>
-        /// <returns>A nullable int.</returns>
-        public int? FooBar(MyClass? bValue)
+        /// <returns>A nullable value.</returns>
+        public MyClass? FooBar(MyClass? bValue)
         {
             return null;
         }
+
+        /// <summary>
+        /// Another extension method.
+        /// </summary>
+        /// <param name="normalBool">A normal MyClass.</param>
+        /// <returns>A normal value.</returns>
+        public static MyClass ExtensionMethod(this MyClass normalBool)
+        {
+            if (normalBool)
+                return 1;
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Just an extension method.
+        /// </summary>
+        /// <param name="nullableBool">A nullable.</param>
+        /// <returns>A Pointer.</returns>
+        public static MyClass* NullableExtensionMethod(this MyClass? nullableBool)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Just an extension method.
+        /// </summary>
+        /// <param name="nullableBool">A nullable.</param>
+        /// <returns>A nullable value.</returns>
+        public static MyClass? NullableExtensionMethod(this MyClass? nullableBool)
+        {
+            return null;
+        }
+
+
     }
 }
